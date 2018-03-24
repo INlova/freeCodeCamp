90d25c71d9911d3b180dd27cdc6c87ab71695eeb
lena-1 <lena@programmer.net>
2018-03-24 07:57:56 -0400

    02-Difference of 2 arrays

--- JS-algorithms/intermediate.js
+++ JS-algorithms/intermediate.js
@@ -1,6 +1,6 @@
 //01-Sum all numbers in a range
-   //Given a list of 2 ranges: [1,4],
-   //sumAll([1,4]) => 1+2+3+4 = 10
+    //Given a list of 2 ranges: [1,4],
+    //sumAll([1,4]) => 1+2+3+4 = 10
 
 
   //1- Via for loop:
@@ -36,3 +36,82 @@
     var max = Math.max.apply(Math, arr);
     return (max - min + 1) * (min + max) / 2;
   }
+
+
+
+//02-Difference of 2 arrays/set operation
+		//Given [1,2,3,4] && [1,2,4], return [3]
+			//diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]) =>  ["piglet", 4]
+			//diffArray([1,2,3,4],[1,2,4])
+
+		//[5,2,9,1].indexOf(3)    //=-1
+		//[5,2,9,1,3].indexOf(3)  //=4
+
+
+	//1- Combine 2 arrays. Filter on this concat array. Wrote as closure.
+	function diffArray(arr1, arr2) {
+	  var joined = arr1.concat(arr2);  //>> [1,2,3,4,1,2,4]
+
+	  function check(num) { //for each num in arr1, arr2
+	    if (arr1.indexOf(num) === -1 || arr2.indexOf(num) === -1) {  //=== -1   = values does not exist
+	      return num; //only return values/items that are not in present in joinedArr
+	    }
+	  }
+	  return joined.filter(check);
+	}
+
+
+	//2- Similar to 1, but we check against each array, not the concat list.
+	function diffArray(arr1, arr2) {
+	  var diffArr = [];
+
+	  function check(listA, listB){
+	    for (var i=0; i<listA.length; i++){
+	      if (listB.indexOf(listA[i]) === -1) {
+	        diffArr.push(listA[i]);
+	      }
+	    }
+	  }
+	  check(arr1, arr2);
+	  check(arr2, arr1);
+	  return diffArr;
+	}
+
+
+	//3- Basic:
+	function diffArray(arr1, arr2) {
+	  var diffArr = [];
+	  for(i = 0; i < arr2.length; i++){
+	    if(arr1.indexOf(arr2[i]) == -1){
+	      diffArr.push(arr2[i]);
+	    }
+	  }
+	  for(j = 0; j < arr1.length; j++){
+	    if(arr2.indexOf(arr1[j]) == -1){
+	      diffArr.push(arr1[j])
+	    }
+	  }
+	  return diffArr;
+	}
+
+
+	//4- Via chained filters:
+	function diffArray(arr1, arr2) {
+	  return arr1.filter(function(val1) {
+	    return (arr2.indexOf(val1) == -1);
+	  }).concat(arr2.filter(function(val2) {
+	    return (arr1.indexOf(val2) == -1);
+	  }));
+	}
+
+
+	//4- re-written for clarity:
+	function diffArray(arr1, arr2) {
+		var firstFiltered =arr1.filter(function(val1) {
+			return (arr2.indexOf(val1) == -1);
+			});
+		var secondFiltered = arr2.filter(function(val2) {
+		    return (arr1.indexOf(val2) == -1);
+			});
+		return firstFiltered.concat(secondFiltered)
+	}
